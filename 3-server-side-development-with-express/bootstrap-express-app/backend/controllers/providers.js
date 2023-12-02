19d1e32338031499bcfd137d115d89e31b252294
Victor Jong <victor.jong@ncs.com.sg>
2023-12-02 16:44:38 +0800

    Update Provider Details

--- 3-server-side-development-with-express/bootstrap-express-app/backend/controllers/providers.js
+++ 3-server-side-development-with-express/bootstrap-express-app/backend/controllers/providers.js
@@ -24,6 +24,18 @@ module.exports.edit = function(req, res) {
 module.exports.update = function(req, res) {
     let id = req.params.id; // Get id from request parameters
     let provider = providers.find(provider => provider.id == id); // Find provider by id
+    provider.firstname = req.body.firstname; // Update provider firstname
+    provider.lastname = req.body.lastname; // Update provider lastname
+    provider.position = req.body.position; // Update provider position
+    provider.company.company_name = req.body.company_name; // Update provider company name
+    provider.company.address = req.body.address; // Update provider company address
+    provider.company.address2 = req.body.address2; // Update provider company address2
+    provider.company.postal_code = req.body.postal_code; // Update provider company postal_code
+    provider.company.phone = req.body.phone; // Update provider company city
+    provider.company.email = req.body.email; // Update provider company state
+    provider.company.description = req.body.description; // Update provider company description
+    provider.company.tagline = req.body.tagline; // Update provider company tagline
+    
     res.render('providers/providers-update', {title: 'Update'});  // Render the providers-edit.hbs view
 }
 
