8a0c1096415201750a078e4674035e2d453956e8
Victor Jong <victor.jong@ncs.com.sg>
2023-12-02 16:37:07 +0800

    edit a provider

--- 3-server-side-development-with-express/bootstrap-express-app/backend/controllers/providers.js
+++ 3-server-side-development-with-express/bootstrap-express-app/backend/controllers/providers.js
@@ -1,11 +1,30 @@
 const providers = require('../models/providers'); // Import providers model
-
+// List Providers
 module.exports.list = function(req, res) {
     res.render('providers/providers-list', {title: 'Service Provider', providers: providers}); // Render the providers-list.hbs view
 }
 
+// Provider Details
 module.exports.details = function(req, res) {
     let id = req.params.id; // Get id from request parameters
     let provider = providers.find(provider => provider.id == id); // Find provider by id
-    res.render('providers/providers-details', {id : id, title: 'Service Provider Details', company: provider.company}); // Render the providers-list.hbs view
+    res.render('providers/providers-details', {id : id, title:
+         'Service Provider Details', company: provider.company}); // Render the providers-list.hbs view
 }
+
+// Edit Provider Details
+module.exports.edit = function(req, res) {
+    let id = req.params.id; // Get id from request parameters
+    let provider = providers.find(provider => provider.id == id); // Find provider by id
+    res.render('providers/providers-edit', {id : id, title:
+         'Service Provider Edit', provider: provider});  // Render the providers-edit.hbs view
+}
+
+// Update Provider Details
+module.exports.update = function(req, res) {
+    let id = req.params.id; // Get id from request parameters
+    let provider = providers.find(provider => provider.id == id); // Find provider by id
+    res.render('providers/providers-update', {title: 'Update'});  // Render the providers-edit.hbs view
+}
+
+
--- 3-server-side-development-with-express/bootstrap-express-app/backend/routes/providers.js
+++ 3-server-side-development-with-express/bootstrap-express-app/backend/routes/providers.js
@@ -8,4 +8,10 @@ router.get('/', providersController.list); // http://localhost:3000/providers/
 /* GET details page. */
 router.get('/details/:id', providersController.details); // http://localhost:3000/providers/details/1
 
+/* GET edit page. */
+router.get('/edit/:id', providersController.edit); // http://localhost:3000/providers/edit/1
+
+/* GET post page. */
+router.post('/update/:id', providersController.update); // http://localhost:3000/providers/update/1
+
 module.exports = router;
