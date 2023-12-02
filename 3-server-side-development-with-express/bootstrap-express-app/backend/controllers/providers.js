5f866baa940951eb9324c3d52af61feab63d99a0
Victor Jong <victor.jong@ncs.com.sg>
2023-12-02 16:17:14 +0800

    service provider details

--- 3-server-side-development-with-express/bootstrap-express-app/backend/controllers/providers.js
+++ 3-server-side-development-with-express/bootstrap-express-app/backend/controllers/providers.js
@@ -3,3 +3,9 @@ const providers = require('../models/providers'); // Import providers model
 module.exports.list = function(req, res) {
     res.render('providers/providers-list', {title: 'Service Provider', providers: providers}); // Render the providers-list.hbs view
 }
+
+module.exports.details = function(req, res) {
+    let id = req.params.id; // Get id from request parameters
+    let provider = providers.find(provider => provider.id == id); // Find provider by id
+    res.render('providers/providers-details', {id : id, title: 'Service Provider Details', company: provider.company}); // Render the providers-list.hbs view
+}
--- 3-server-side-development-with-express/bootstrap-express-app/backend/routes/providers.js
+++ 3-server-side-development-with-express/bootstrap-express-app/backend/routes/providers.js
@@ -5,5 +5,7 @@ const providersController = require('../controllers/providers'); // Import provi
 /* GET list page. */
 router.get('/', providersController.list); // http://localhost:3000/providers/
 
+/* GET details page. */
+router.get('/details/:id', providersController.details); // http://localhost:3000/providers/details/1
 
 module.exports = router;
