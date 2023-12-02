955279abfac201b357a84f8219a891168d4abfb1
Victor Jong <victor.jong@ncs.com.sg>
2023-12-02 22:44:10 +0800

    creating schemas and models

--- 4-getting-started-with-mongodb/bootstrap-express-app-api-mongodb/backend/api/db/db.js
+++ 4-getting-started-with-mongodb/bootstrap-express-app-api-mongodb/backend/api/db/db.js
@@ -1,13 +1,16 @@
 const mongoose = require('mongoose');
+const {Provider} = require('../models/provider');
 
 // connection uri to mongodb
 const uri = 'mongodb://localhost:27017/provider_db';
 
 // make db connection (asynchronously)
-module.exports = mongoose.connect(uri)
+mongoose.connect(uri)
     .then(result => {
         console.log('Connected to MongoDB');
     })
     .catch(error => {
         console.log('Error connecting to MongoDB', error);
     });
+
+module.exports = Provider; // Export model for use in other modules
