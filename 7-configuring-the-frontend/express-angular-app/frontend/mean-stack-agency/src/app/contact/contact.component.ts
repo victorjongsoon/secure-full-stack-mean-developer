ebaf315a33bf62ba5fe3ee0ffb04b6cda266afc4
Victor Jong <victor.jong@ncs.com.sg>
2023-12-03 18:18:38 +0800

    template driven form contact form

--- 7-configuring-the-frontend/express-angular-app/frontend/mean-stack-agency/src/app/contact/contact.component.ts
+++ 7-configuring-the-frontend/express-angular-app/frontend/mean-stack-agency/src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,5 +8,11 @@ import { Component } from '@angular/core';
 })
 
 export class ContactComponent {
+  constructor() { }
+  ngOnInit() { }
 
+  handleSubmit(contactForm: NgForm){
+    console.log(contactForm.value);
+    console.log(contactForm.value.message);
+  }
 }
